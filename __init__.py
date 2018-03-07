ff110808ee912050dd2bec5039e71d3b4fe99099
vdatasci <joshvoss90@outlook.com>
2018-03-07 10:31:50 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,9 +6,10 @@ response = request.get('https://feedwm.org/faqs/click-here-for-our-mobile-food-p
 html = response.content
 soup = bs(html, 'html.parser')
 
-anchorContents = [str(link.string) for link in soup.findAll('a')]
+#anchorContents = [str(link.string) for link in soup.findAll('a')]
+#GRmatches = filter(re.compile('.*Grand Rapids.*').match, anchorContents)
+#indGRmatches = [i for i,val in enumerate(anchorContents) if re.match('.*Grand Rapids.*', val)]
+#[anchorContents[int(i) for i in indGRmatches]
 
-GRmatches = filter(re.compile('.*Grand Rapids.*').match, anchorContents)
-indGRmatches = [i for i,val in enumerate(anchorContents) if re.match('.*Grand Rapids.*', val)]
-
-[anchorContents[int(i) for i in indGRmatches]
+lis = [li.text for li in soup.findAll('li')]
+lis = [x.encode('utf-8').strip() for x in lis]
