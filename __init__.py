4db39993d7d1d23065c0afcdeed1740560619b04
vdatasci <joshvoss90@outlook.com>
2018-03-08 10:57:06 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -15,22 +15,27 @@ soup = bs(html, 'html.parser')
 #indGRmatches = [i for i,val in enumerate(anchorContents) if re.match('.*Grand Rapids.*', val)]
 #[anchorContents[int(i) for i in indGRmatches]
 
-lis = [li.text for li in soup.findAll('li')]
-lis = [x.encode('utf-8').strip() for x in lis]
+#lis = [li.text for li in soup.findAll('li')]
+#lis = [x.encode('utf-8').strip() for x in lis]
 
-grtimes = filter(re.compile('[0-9]:[0-9]').match, lis)
+#grtimes = filter(re.compile('[0-9]:[0-9]').match, lis)
 
-filter(re.compile('[0-9]+..[0-9]').match, grtimes)
+#filter(re.compile('[0-9]+..[0-9]').match, grtimes)
+
+tags = [tag.text for tag in soup.findAll(['h3','li'])]
+tags = [tag.encode('utf-8').strip() for tag in tags]
+ffdata = filter(re.compile('[0-9]:[0-9]|\w+.\d+\,.\d+').match, tags)
 
 
-# Example of datetime parser
-parser.parse('March 7, 2018 4:00 PM')
+
+## Example of datetime parser
+#parser.parse('March 7, 2018 4:00 PM')
 
 
-h3s = [h3.text for h3 in soup.findAll('h3')]
-h3s = [h3.encode('utf-8').strip() for h3 in h3s]
-grdates = filter(re.compile('\w+.\d+\,.\d+').match, h3s)
-grdates = [parser.parse(str(d)).strftime('%Y-%m-%d') for d in grdates]
+#h3s = [h3.text for h3 in soup.findAll('h3')]
+#h3s = [h3.encode('utf-8').strip() for h3 in h3s]
+#grdates = filter(re.compile('\w+.\d+\,.\d+').match, h3s)
+#grdates = [parser.parse(str(d)).strftime('%Y-%m-%d') for d in grdates]
 
 
 #2018-03-6T10:00:00+02:00
